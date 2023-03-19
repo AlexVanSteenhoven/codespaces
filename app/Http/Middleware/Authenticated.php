ca081071ac91dc790786237459a71e8ca1c2afcc
Alex van Steenhoven <alex.vs@fullstak.nl>
2023-03-19 01:41:52 +0100

    Created a new auth middleware

--- app/Http/Middleware/Authenticated.php
+++ app/Http/Middleware/Authenticated.php
@@ -3,18 +3,25 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class Authenitcated
+class Authenticated
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param Request $request
+     * @param Closure $next
+     * @return Response|RedirectResponse
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next): Response|RedirectResponse
     {
+        if (auth()->guest()) {
+            return redirect()->route('auth.login')->with('error', 'You must be authenticated to access this resource!');
+        }
+
         return $next($request);
     }
 }
