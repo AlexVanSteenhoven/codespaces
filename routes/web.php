b543000873f2c4a1b13bbd931009c6ba0545e321
Alex van Steenhoven <alex.vs@fullstak.nl>
2023-03-27 22:52:14 +0200

    Clean up routes file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,11 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () { return view('pages.home'); })->name('home');
-Route::get('/dashboard', function () { return view('pages.dashboard'); });
+Route::view('/','pages.home')->name('home');
 
 // Authentication Routes
 Route::get('/sign-in', [AuthenticationController::class, 'index'])->name('auth.login');
 Route::get('/sign-up', [AuthenticationController::class, 'create'])->name('auth.register');
 Route::get('/sign-in/{provider}/redirect', [AuthenticationController::class, 'redirect'])->name('auth.redirect');
 Route::get('/sign-in/{provider}/callback', [AuthenticationController::class, 'callback'])->name('auth.callback');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/dashboard', [ DashboardController::class, 'index']);
+});
+
