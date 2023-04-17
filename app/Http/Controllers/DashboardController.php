1878688e7e2e2b6f17db8daf524c20f67c5b656c
Alex van Steenhoven <alex.vs@fullstak.nl>
2023-04-18 00:39:57 +0200

    Added spatie/laravel-permissions with initial setup

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Redis;
+
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index');
+        return view('pages.dashboard.index');
     }
 }
