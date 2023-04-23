7ac33e88b4aa5ca50435db2604f005e5858f0e14
Alex van Steenhoven <alex.vs@fullstak.nl>
2023-04-23 23:12:59 +0200

    Generated ide/model helpers

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,11 +2,48 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Client
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $company
+ * @property string $email
+ * @property string $phone
+ * @property string $address
+ * @property string $status
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection<int, Comment> $comments
+ * @property-read int|null $comments_count
+ * @property-read Collection<int, Project> $projects
+ * @property-read int|null $projects_count
+ * @property-read Collection<int, Task> $tasks
+ * @property-read int|null $tasks_count
+ * @property-read User|null $user
+ * @method static Builder|Client newModelQuery()
+ * @method static Builder|Client newQuery()
+ * @method static Builder|Client query()
+ * @method static Builder|Client whereAddress($value)
+ * @method static Builder|Client whereCompany($value)
+ * @method static Builder|Client whereCreatedAt($value)
+ * @method static Builder|Client whereEmail($value)
+ * @method static Builder|Client whereId($value)
+ * @method static Builder|Client whereName($value)
+ * @method static Builder|Client wherePhone($value)
+ * @method static Builder|Client whereStatus($value)
+ * @method static Builder|Client whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class Client extends Model
 {
     use HasFactory;
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,11 +2,40 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Task
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $description
+ * @property string $status
+ * @property int $user_id
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection<int, Comment> $comments
+ * @property-read int|null $comments_count
+ * @property-read User $user
+ * @method static Builder|Task newModelQuery()
+ * @method static Builder|Task newQuery()
+ * @method static Builder|Task query()
+ * @method static Builder|Task whereCreatedAt($value)
+ * @method static Builder|Task whereDescription($value)
+ * @method static Builder|Task whereId($value)
+ * @method static Builder|Task whereStatus($value)
+ * @method static Builder|Task whereTitle($value)
+ * @method static Builder|Task whereUpdatedAt($value)
+ * @method static Builder|Task whereUserId($value)
+ * @mixin Eloquent
+ */
 class Task extends Model
 {
     use HasFactory;
