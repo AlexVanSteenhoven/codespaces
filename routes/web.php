d202f425ba0ea0f0067b0f64f8716bc5c1d11593
Alex van Steenhoven <alex.vs@fullstak.nl>
2023-04-23 23:14:06 +0200

    Grouped auth routes

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Return the homepage
 Route::view('/','pages.home')->name('home');
 
-// Authentication Routes
-Route::get('/sign-in', [AuthenticationController::class, 'index'])->name('auth.login');
-Route::get('/sign-up', [AuthenticationController::class, 'create'])->name('auth.register');
-Route::get('/sign-out', [AuthenticationController::class, 'logout'])->name('auth.logout');
-Route::post('/sign-in/email', [AuthenticationController::class, 'login'])->name('auth.sign-in'); // TODO :: Fix breaking bug (redirect to 419 page expired)
-Route::get('/sign-in/{provider}/redirect', [AuthenticationController::class, 'redirect'])->name('auth.redirect');
-Route::get('/sign-in/{provider}/callback', [AuthenticationController::class, 'callback'])->name('auth.callback');
+Route::name('auth.')->prefix('auth')->group(function () {
+    Route::get('/login', [AuthenticationController::class, 'index'])->name('login');
+    Route::get('/register', [AuthenticationController::class, 'create'])->name('register');
+    Route::get('/logout', [AuthenticationController::class, 'logout'])->name('logout');
+    Route::post('/login/email', [AuthenticationController::class, 'login'])->name('login.email');
+    Route::get('/login/{provider}/redirect', [AuthenticationController::class, 'redirect'])->name('redirect');
+    Route::get('/login/{provider}/callback', [AuthenticationController::class, 'callback'])->name('callback');
+});
 
 Route::middleware(['auth'])->group(function () {
     Route::namespace('App')->prefix('app')->name('app.')->group(function () {
