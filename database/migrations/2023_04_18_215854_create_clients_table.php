1bc82640a1901723f6a479a80659efb99f887861
Alex van Steenhoven <alex.vs@fullstak.nl>
2023-04-23 20:16:47 +0200

    added migrations

--- database/migrations/2023_04_18_215854_create_clients_table.php
+++ database/migrations/2023_04_18_215854_create_clients_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Client;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('company');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('status')->default(Client::STATUS_ACTIVE);
             $table->timestamps();
         });
     }
